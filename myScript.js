ebf72116767b6548ce66b51531b7ef84b6e67eed
Matt <matthuynh24@gmail.com>
2022-07-05 16:28:18 -0700

    Created a 16 x 16 grid inside of the container with a for loop

---
+++ myScript.js
@@ -0,0 +1,12 @@
+const container = document.getElementById("container");
+
+function createGrid(rows, cols) {
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+    for (c = 0; c < (rows * cols); c++) {
+        let cell = document.createElement("div");
+        container.appendChild(cell).className = "grid-item";
+    };
+};
+
+createGrid(16, 16);
